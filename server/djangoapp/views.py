3747120f3618e683e8b3e5fd97e54872e6adbb9c
stutimishra <stutimishra1999dec@gmail.com>
2025-04-22 08:24:25 -0400

    capestone

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -97,7 +97,7 @@ def get_dealerships(request, state="All"):
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 # def get_dealer_reviews(request,dealer_id):
-# ...def get_dealer_reviews(request, dealer_id):
+def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
     if(dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -20,6 +20,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
