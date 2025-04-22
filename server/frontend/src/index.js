f721312ad0206dfca2cd164506dac81faa306c2b
stutimishra <stutimishra1999dec@gmail.com>
2025-04-22 11:24:14 -0400

    push cahnges

--- server/frontend/src/index.js
+++ server/frontend/src/index.js
@@ -1,16 +1,17 @@
-/* jshint esversion: 6, browser: true, strict: true */
+/* jshint esversion: 6, browser: true */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
 (function () {
   'use strict';
 
-  import React from 'react';
-  import ReactDOM from 'react-dom/client';
-  import App from './App';
-  import { BrowserRouter } from 'react-router-dom';
-
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    React.createElement(BrowserRouter, null,
+      React.createElement(App, null)
+    )
   );
 })();
